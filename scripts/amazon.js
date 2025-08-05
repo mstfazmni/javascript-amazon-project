557c24d0d650616d0ef7ce2e293c4c0874288c4c
Mostafa Zamani <119905696+mstfazmni@users.noreply.github.com>
2025-08-04 21:40:59 -0600

    Fic price formatting and radio selectors

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,6 @@
 import { products } from '../data/products.js';
 import { cart, addToCart } from '../data/cart.js';
+import { formatCurrency } from './utils/money.js';
 
 // import {cart as myCart} from '../data/cart.js';
 //for naming conflicts if we had another variable in this file with the same name!
@@ -28,7 +29,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-price">
-            $${(product.priceCents / 100).toFixed(2)}
+            $${formatCurrency(product.priceCents)}
           </div>
 
           <div class="product-quantity-container">
