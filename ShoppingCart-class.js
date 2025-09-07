cf0a4df37622cb4972ffae56fe7fc4bea28d95e3
Mostafa Zamani <119905696+mstfazmni@users.noreply.github.com>
2025-09-06 20:00:22 -0600

    finish Shopping Cart class

--- ShoppingCart-class.js
+++ ShoppingCart-class.js
@@ -1,33 +1,47 @@
 class ShoppingCart {
     //Properties
-    name;
-    price;
-    quantity;
+    items = [];
 
     // Constroctor which is setup code
-    constructor(name, price, quantity) {
-        this.name = name;
-        this.price = price;
-        this.quantity = quantity;
+    constructor() {
+        this.items = [];
     }
 
     // Mehtods
-    AddItem() {
+    AddItem(name, price, quantity = 1) {
+        const existing = this.items.find(i => i.name === name);
+        if (existing) {
+            existing.quantity += quantity;
+        } else {
+            this.items.push({name, price, quantity});
+        }
 
+        return this.items;
     }
 
-    RemoveItem() {
-
+    RemoveItem(name) {
+        const idx = this.items.findIndex(i => i.name === name)
+        if(idx === -1) return false;
+        this.items.splice(idx, 1);
+        return this.items;
     }
 
     CalculateTotal() {
-
+        const sum = this.items.reduce((acc, item) => acc + item.quantity * item.price , 0);
+        return Math.round(sum * 100) / 100;
     }
 
     ClearCart() {
-
+        this.items = [];
     }
 }
 
-const cart = new ShoppingCart("Apple", 20, 3);
-console.log(cart);
+const cart1 = new ShoppingCart();
+
+cart1.AddItem("Apple", 14, 2);
+cart1.AddItem("Banana", 11, 5);
+cart1.AddItem("Peanuts", 4, 15);
+
+
+// cart1.RemoveItem("Banana");
+console.log("Total:", cart1.CalculateTotal());
