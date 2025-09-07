26d66afdf6d9bba3c49db4ff90c9d9a3b6e0ba07
Mostafa Zamani <119905696+mstfazmni@users.noreply.github.com>
2025-09-06 19:18:28 -0600

    create deposit and withdraw methods for our class

--- BankAccount-class.js
+++ BankAccount-class.js
@@ -6,6 +6,26 @@ class BankAccount {
         if(initialBalance < 0) throw new Error("Initial balance can't be negative");
         this.#balance = initialBalance;
     }
+
+    deposit(amount) {
+        if (typeof amount !== "number" || amount <= 0) {
+            throw new Error("Deposit amount must be a positive number");
+        }
+        this.#balance += amount;
+        return this.#balance;
+    }
+
+    withdraw(amount) {
+        if (typeof amount !== "number" || amount <= 0) {
+            throw new Error("withdraw amount must be a positive number");
+        }
+        if (amount > this.#balance) {
+            throw new Error("Insufficient funds");
+        }
+        this.#balance -= amount;
+        return this.#balance;
+    }
 }
 
-const BankAccount = new BankAccount();
+const BankAccount1 = new BankAccount('Mostafa', 100);
+console.log(BankAccount1)
