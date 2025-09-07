810fa511dba8d87c11a9f82c4ecce8ab15e717d2
Mostafa Zamani <119905696+mstfazmni@users.noreply.github.com>
2025-09-06 19:22:26 -0600

    create a simple class for practice

--- BankAccount-class.js
+++ BankAccount-class.js
@@ -1,31 +1,36 @@
 class BankAccount {
-    #balance = 0;
+    balance = 0;
 
     constructor(owner, initialBalance = 0) {
         this.owner = owner;
         if(initialBalance < 0) throw new Error("Initial balance can't be negative");
-        this.#balance = initialBalance;
+        this.balance = initialBalance;
     }
 
     deposit(amount) {
         if (typeof amount !== "number" || amount <= 0) {
             throw new Error("Deposit amount must be a positive number");
         }
-        this.#balance += amount;
-        return this.#balance;
+        this.balance += amount;
+        return this.balance;
     }
 
     withdraw(amount) {
         if (typeof amount !== "number" || amount <= 0) {
             throw new Error("withdraw amount must be a positive number");
         }
-        if (amount > this.#balance) {
+        if (amount > this.balance) {
             throw new Error("Insufficient funds");
         }
-        this.#balance -= amount;
-        return this.#balance;
+        this.balance -= amount;
+        return this.balance;
     }
 }
 
-const BankAccount1 = new BankAccount('Mostafa', 100);
-console.log(BankAccount1)
+const a = new BankAccount('Mostafa', 100);
+console.log(a.balance);
+a.deposit(50);
+console.log(a.balance);
+
+a.withdraw(75);
+console.log(a.balance)
